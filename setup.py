c76dc5eb34fb9279d7a44110b5a7876b7dafa5b2
simon.grafenhorst <grafenhorst@kit.edu>
2023-04-21 15:03:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 Setup information for the project.
-
-Code initally generated for the Praktikum: Smart Energy System Lab.
 """
 from setuptools import find_packages, setup
 
 setup(
-    name="22_p4_building_model",
+    name="house_model",
     version="0.1.0",
-    description="Project to model the electricity house of Energy Lab 2.0",
-    author="Anja Hagen & Jan Ludwig",
+    description="Electricity grid model of a house at KIT Energy Lab 2.0",
+    author="Anja Hagen, Jan Ludwig, Simon Grafenhorst",
     packages=find_packages(exclude=("tests", "docs")),
+    install_requires=["pandapower",],
 )
 
